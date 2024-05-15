b084dc288517151d351c3a0f367922229ad0c80c
Paul31770 <paul.laffont31770@gmail.com>
2024-05-15 10:58:14 +0200

    modif controller

--- src/main/java/com/example/server_basket/controller/matchController.java
+++ src/main/java/com/example/server_basket/controller/matchController.java
@@ -1,23 +1,45 @@
 package com.example.server_basket.controller;
 
+import com.example.server_basket.dto.inputMatchDto;
 import com.example.server_basket.entity.matchEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.example.server_basket.service.matchService;
+import org.springframework.ui.Model;
 import java.util.List;
 
-@RestController
+@Controller
 public class matchController {
     @Autowired
     private matchService matchService;
     @GetMapping("/hello")
     public String hello(){
-        return "bonjour";
+        return "index";
     }
 
     @GetMapping("/getAll")
-    public List<matchEntity> getAll(){
-        return matchService.getAll();
+    public String getAll(Model model){
+        /*return matchService.getAll();*/
+        List<matchEntity> matches = matchService.getAll();
+        System.out.println(matches);
+        model.addAttribute("matches", matches);
+        return "index";
+    }
+
+    @PostMapping("/addMatch")
+    public ResponseEntity addMatch(@RequestBody inputMatchDto dto){
+        return new ResponseEntity(matchService.addMacth(dto), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/recoMatchFinish")
+    public List<matchEntity> recoMatchFinish(){
+
+        return matchService.getAllMatchFinish();
     }
 }
--- src/main/java/com/example/server_basket/service/matchService.java
+++ src/main/java/com/example/server_basket/service/matchService.java
@@ -1,10 +1,15 @@
 package com.example.server_basket.service;
 
+import com.example.server_basket.dto.inputMatchDto;
 import com.example.server_basket.repository.matchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.server_basket.entity.matchEntity;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +21,33 @@ public class matchService {
         return matchRepo.findAll();
     }
 
+    public Integer addMacth(inputMatchDto dto){
+        LocalDate date= LocalDate.parse(dto.getDate(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        matchEntity match= new matchEntity();
+        match.setName_team1(dto.getName_team1());
+        match.setName_team2(dto.getName_team2());
+        match.setScore_team1(dto.getScore_team1());
+        match.setScore_team2(dto.getScore_team2());
+        match.setStatus(dto.getStatus());
+        match.setDate(date);
+        match.setLogo_team1(dto.getLogo_team1());
+        match.setLogo_team2(dto.getLogo_team2());
+
+        matchRepo.saveAndFlush(match);
+        return match.getID();
+    }
+
     public List<matchEntity> getAllMatchFinish(){
+        LocalDate date= LocalDate.now();
+        List<matchEntity> listMatchFinish= new ArrayList<>();
+        List<matchEntity> listAllMatch= matchRepo.findAll();
+        for ( matchEntity match : listAllMatch){
+            Long betweenDate = ChronoUnit.DAYS.between(date,match.getDate());
+            if (!(betweenDate>= -7) || match.getStatus()=="terminé"){
+                listMatchFinish.add(match);
+            }
+        }
+        System.out.println(listMatchFinish);
 
         return null;
     }
